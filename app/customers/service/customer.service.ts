16e206f6572a281a8cd9225b81525d93932a1fd6
Pedro Tatibano <pedrotatibano190@gmail.com>
2025-03-17 01:37:27 -0300

    feat: add customer management functionality with CRUD operations and validation

---
+++ app/customers/service/customer.service.ts
@@ -0,0 +1,29 @@
+import api from "../../../services/api/apiConfig";
+import { Customer } from "./entity/customers.entity";
+
+export const findAllCustomers = async (): Promise<Customer[]> => {
+  const response = await api.get("/clients");
+  return response.data.data;
+};
+
+export const findOneCustomer = async (id: string): Promise<Customer> => {
+  const response = await api.get(`/clients/${id}`);
+  return response.data.data;
+};
+
+export const createCustomer = async (customer: Customer): Promise<Customer> => {
+  const response = await api.post("/clients", customer);
+  return response.data.data;
+};
+
+export const updateCustomer = async (
+  id: string,
+  data: Partial<Customer>
+): Promise<Customer> => {
+  const response = await api.patch(`/clients/${id}`, data);
+  return response.data.data;
+};
+
+export const deleteCustomer = async (id: string): Promise<void> => {
+  await api.delete(`/clients/${id}`);
+};
