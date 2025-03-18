cf40691f34ab24f02cd01026fd65d9adc2daf372
Pedro Tatibano <pedrotatibano190@gmail.com>
2025-03-18 00:47:12 -0300

    feat: implement pagination and sorting for customer list, update customer entity imports

--- app/customers/components/modals/NewCustomerModal.tsx
+++ app/customers/components/modals/NewCustomerModal.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Customer } from "../../service/entity/customers.entity";
+import { Customer } from "../../entities/customers.entity";
 
 interface NewCustomerModalProps {
   isOpen: boolean;
