e331387664bb693ad6c0e46cba7602865d67492c
Pedro Tatibano <pedrotatibano190@gmail.com>
2025-03-20 00:01:48 -0300

    feat: add NewCreditPurchaseModal component for creating credit purchases

--- app/credit-purchases/components/modals/NewCreditPurchaseModal.tsx
+++ app/credit-purchases/components/modals/NewCreditPurchaseModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { z } from "zod";
-import { Customer } from "../../types";
+import { Customer } from "../../../types";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
